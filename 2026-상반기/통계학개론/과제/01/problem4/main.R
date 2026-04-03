476372cc7d077016dcb72983f4e16a2550ba9493
JongChan Choi <jong@chan.moe>
2026-04-03 16:59:01 +0900

    Add problem 4 solution to statistics assignment

---
+++ 2026-상반기/통계학개론/과제/01/problem4/main.R
@@ -0,0 +1,22 @@
+set.seed(2026)
+
+p <- 0.7
+
+sample_sizes <- c(10, 20, 50, 75, 100, 200)
+n_reps <- 1000
+
+for (n in sample_sizes) {
+  sample_props <- numeric(n_reps)
+  for (i in 1:n_reps) {
+    sample_data <- sample(
+      c(1, 0),
+      size = n,
+      replace = TRUE,
+      prob = c(p, 1 - p)
+    )
+    sample_props[i] <- mean(sample_data)
+  }
+  cat("표본 크기:", n, "\n")
+  cat("표본비율 평균:", mean(sample_props), "\n")
+  cat("표본비율 표준편차:", sd(sample_props), "\n\n")
+}
